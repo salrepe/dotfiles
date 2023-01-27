ab5587bd9579d7d913fac3f57df8903839fbda9f
salrepe <salrepe@gmail.com>
2023-01-27 12:45:32 +0100

    Update zim modules and config

--- zsh/aliases/apps.sh
+++ zsh/aliases/apps.sh
@@ -3,32 +3,18 @@
 app() {
   if [ -d "${HOME}/peertransfer/code/$1" ]; then
     cd "${HOME}/peertransfer/code/$1"
-  elif [ -d "/home/vagrant/code/platform/apps/$1" ]; then
-    cd "/home/vagrant/code/platform/apps/$1"
-  elif [ -d "${HOME}/peertransfer/code/platform/apps/$1" ]; then
-    cd "${HOME}/peertransfer/code/platform/apps/$1"
   else
     echo "404 app not found :("
   fi
 }
 
-iapp() {
-  dirtomove=$(ls "${HOME}/peertransfer/code" "${HOME}/peertransfer/code/platform/apps" | fzf)
-  app $dirtomove
-}
-
-infra() {
-  if [ -d "/home/vagrant/code/platform/infra/$1" ]; then
-    cd "/home/vagrant/code/platform/infra/$1"
-  else
-    cd "${HOME}/peertransfer/code/platform/infra/$1"
+function merge-to-monoenv() {
+  if [ $# -ne 1 ]; then
+    echo "usage: merge-to-monoenv <environment>"
+    return -1
   fi
-}
-
-platform() {
-  if [ -d "/home/vagrant/code/platform" ]; then
-    cd "/home/vagrant/code/platform"
-  else
-    cd "${HOME}/peertransfer/code/platform"
-  fi
+  ENVIRONMENT=$1
+  BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  git checkout $ENVIRONMENT && git pull origin $ENVIRONMENT && git merge $BRANCH && git commit --allow-empty -m "[SKIP_MANUAL]"
+  git checkout $BRANCH
 }
